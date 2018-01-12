ea0d06e95db6f807e55c15e92cf71cdc6f00f128
Scott Moon <jiwonsis@naver.com>
2018-01-13 01:59:49 +0900

    API 추가 및 에러 처리 수정
    - API 추가 : 이메일 체크
    - 에러 처리 : 에러가 났을 경우, json으로 에러 상태 내보내게 설정함

--- src/api/v1.0/auth/auth.ctrl.js
+++ src/api/v1.0/auth/auth.ctrl.js
@@ -1,6 +1,24 @@
 const Joi = require('joi');
 const User = require('db/models/User');
 
+exports.checkEmail = async (ctx) => {
+  const { email } = ctx.params;
+
+  if (!email) {
+    ctx.status = 400;
+    return;
+  }
+
+  try {
+    const account = await User.findByEmail(email);
+    ctx.body = {
+      exists: !!account // !: change boolean type || !!: null check null ? false : true
+    };
+  } catch (e) {
+    ctx.throw(e, 500);
+  }
+};
+
 // register logic
 exports.localRegister = async (ctx) => {
   const { body } = ctx.request;
@@ -51,7 +69,7 @@ exports.localRegister = async (ctx) => {
       maxAge: 1000 * 60 * 60 * 24 * 7
     });
   } catch (e) {
-    ctx.throw(500);
+    ctx.throw(e, 500);
   }
 };
 
@@ -100,7 +118,7 @@ exports.localLogin = async (ctx) => {
       metaInfo
     };
   } catch(e) {
-    ctx.throw(e);
+    ctx.throw(e, 500);
   }
 };
 
